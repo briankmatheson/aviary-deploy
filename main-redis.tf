00cc088ff2ec3667b05d7a32473f5e1727dc5f2f
ameen-neslit-252 <ameen@nesl-it.com>
2025-04-03 20:14:39 +0500

    feat/added redis service

---
+++ main-redis.tf
@@ -0,0 +1,17 @@
+resource "helm_release" "redis" {
+  name       = "redis"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "redis"
+  namespace  = var.redis_namespace
+  create_namespace = true
+
+  set {
+    name  = "auth.enabled"
+    value = var.redis_auth_enabled
+  }
+
+  set {
+    name  = "architecture"
+    value = var.redis_architecture
+  }
+}
